3e14ff118771016b6352b9f9c65f211dacffa498
Danny Sung <danny@dannysung.com>
2020-09-25 16:36:13 -0700

    Created TestHostApp

---
+++ SomeFramework/TestHostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHostApp
+//
+//  Created by Sung, Danny on 9/25/20.
+//  Copyright © 2020 Sung, Danny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
