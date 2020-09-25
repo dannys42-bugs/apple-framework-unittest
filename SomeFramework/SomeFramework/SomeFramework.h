cc420fe15505b1ea35553286d48ce54fea91f9e9
Danny Sung <danny@dannysung.com>
2020-09-25 16:33:23 -0700

    Created template framework with unit test with Xcode 11.7

---
+++ SomeFramework/SomeFramework/SomeFramework.h
@@ -0,0 +1,19 @@
+//
+//  SomeFramework.h
+//  SomeFramework
+//
+//  Created by Sung, Danny on 9/25/20.
+//  Copyright © 2020 Sung, Danny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SomeFramework.
+FOUNDATION_EXPORT double SomeFrameworkVersionNumber;
+
+//! Project version string for SomeFramework.
+FOUNDATION_EXPORT const unsigned char SomeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SomeFramework/PublicHeader.h>
+
+
